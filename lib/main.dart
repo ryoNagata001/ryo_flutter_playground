65e588bef8a349f1618c7412c97fb6a59b75cdda
ryo.nagata <ryo.nagata@chillnn.com>
2022-01-29 13:55:03 +0900

    end of auto layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     // タイトル
     Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Container(
                   padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text('Oeschinen Lake Campground',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ))),
-              Text('Kandersteg, Switzerland', style: TextStyle(color: Colors.grey[500]))
-            ]),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41')
-        ],
-      )
-    );
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    )
+                  )
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500]
+                  )
+                )
+              ]),
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.red[500],
+            ),
+            const Text('41')
+          ],
+        )
+      );
 
     // ボタンセクション
     Color color = Theme.of(context).primaryColor;
@@ -47,23 +54,47 @@ class MyApp extends StatelessWidget {
       ],
     );
 
+    // テキストセクション
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
-        title: 'Flutter layout demo',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
+      title: 'Flutter layout demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo')
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Flutter layout demo')
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
             ),
-            body: Column(
-              children: [
-                titleSection,
-                buttonSection,
-              ],
-            )));
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        )
+      )
+    );
   }
 
+  // ボタンカラム
   Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
